479ebd6ffae169d8c47dc37db434b232fb35c86a
Adrian Rangel <adrian.rangel@gmail.com>
2014-10-30 09:21:20 -0600

    use student_id for login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,14 @@
 class RegistrationsController < Devise::RegistrationsController
+  def create
+    if params[:user][:email].match(/[aAlL]\d{8}/)
+      params[:user][:email] = "#{params[:user][:email]}@itesm.mx"
+      super
+    else
+      flash.now[:alert] = 'No es una matricula valida'
+      redirect_to root_path
+    end
+  end
+
   protected
 
   def after_sign_up_path_for(resource)
