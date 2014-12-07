d5451ba6a86a9f393cde479b5bb08f1cbd78d4fa
Adrian Rangel <adrian.rangel@gmail.com>
2014-12-06 22:27:26 -0600

    logout on layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   skip_before_filter :authenticate_user!
+  skip_before_filter :check_first_diagnostic_exam
 
   def index
     redirect_to dashboard_path if user_signed_in?
