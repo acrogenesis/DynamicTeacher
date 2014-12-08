3bd5fa78f7ac90abb074b99c01e42fcadeb97515
Adrian Rangel <adrian.rangel@gmail.com>
2014-12-08 08:18:46 -0600

    first diagnostic exam schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,39 +10,110 @@ puts 'Creando Examenes Diagnosticos...'
 Subject.subjects.each do |subject|
   DiagnosticExam.create(subject: subject) if DiagnosticExam.where(subject: subject).empty?
 end
+# {
+#   subject: 'basic_concepts',
+#   questions_attributes: {
+#     question: 'Los siguientes son tipos de datos en C#',
+#     answer_attributes: {
+#       answer: 'bool',
+#       field_type: 'check_box',
+#       correct: true
+#     }
+#   }
+# }
+d = DiagnosticExam.where(subject: 'basic_concepts').first
+q = Question.create(question: 'Los siguientes son tipos de datos en C#', diagnostic_exam_id: d.id)
+Answer.create(answer: 'bool', field_type: 'check_box', correct: true, question_id: q.id)
+Answer.create(answer: 'string', field_type: 'check_box', correct: true, question_id: q.id)
+Answer.create(answer: 'long', field_type: 'check_box', correct: true, question_id: q.id)
+Answer.create(answer: 'decimal', field_type: 'check_box', correct: true, question_id: q.id)
+Answer.create(answer: 'char', field_type: 'check_box', correct: true, question_id: q.id)
+Answer.create(answer: 'short', field_type: 'check_box', correct: true, question_id: q.id)
 
-puts 'Creando Tareas...'
-homeworks = Dir[File.join('db/homeworks/*')].sort
+q = Question.create(question: 'Una variable local', diagnostic_exam_id: d.id)
+Answer.create(answer: 'Puede ser utilizada en cualquier lugar del programa', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'Se declara dentro de un método (main)', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: 'Debe ser instanciada desde su declaración', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'Ninguna de las anteriores', field_type: 'radio_button', correct: false, question_id: q.id)
 
-homeworks.each do |homework|
-  subject, level = File.basename(homework).split('-')
-  Homework.create(subject: subject, level: level, description: File.read(homework)) if Homework.where(subject: subject, level: level, description: File.read(homework)).empty?
-end
+q = Question.create(question: 'El método Console.ReadLine() se utiliza para leer el input del usuario en consola', diagnostic_exam_id: d.id)
+Answer.create(answer: 'Falso', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'Verdadero', field_type: 'radio_button', correct: true, question_id: q.id)
 
-puts 'Creando Videos...'
-videos = [
-  {
-    url: '//www.youtube.com/embed/T4RPkiXjqd8',
-    subject: 'ifs',
-    title: 'Estatuos de Decision #1'
-  },
-  {
-    url: '//www.youtube.com/embed/iFJ8xRgRoXg',
-    subject: 'switches',
-    title: 'Estatuos de Decision #2'
-  },
-  {
-    url: '//www.youtube.com/embed/xAQiw-uoNeE',
-    subject: 'cycles',
-    title: 'Ciclos #1'
-  },
-  {
-    url: '//www.youtube.com/embed/OfT0585DZUg',
-    subject: 'cycles',
-    title: 'Ciclos #2'
-  }
-]
+q = Question.create(question: 'Una variable global', diagnostic_exam_id: d.id)
+Answer.create(answer: 'Puede ser utilizada en cualquier lugar del programa', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: 'Se declara dentro de un método (main)', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'Debe ser instanciada desde su declaración', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'Ninguna de las anteriores', field_type: 'radio_button', correct: false, question_id: q.id)
 
-videos.each do |video|
-  Video.create(video) if Video.where(video).empty?
-end
+q = Question.create(question: 'Al momento de declarar una variable, siempre se debe de instanciar (inicializar)', diagnostic_exam_id: d.id)
+Answer.create(answer: 'Falso', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: 'Verdadero', field_type: 'radio_button', correct: false, question_id: q.id)
+
+q = Question.create(question: 'Método utilizado para convertir de un String a un número entero', diagnostic_exam_id: d.id)
+Answer.create(answer: 'texto.toInteger()', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'parse(texto)', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: 'parse(texto)', field_type: 'radio_button', correct: false, question_id: q.id)
+
+q = Question.create(question: 'Diferencia entre las comillas simples (\') y las dobles (")', diagnostic_exam_id: d.id)
+Answer.create(answer: 'No hay diferencia', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'Las comillas simples son para caracteres y las dobles para strings', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: 'Las comillas simples son para strings y las dobles para caracteres', field_type: 'radio_button', correct: false, question_id: q.id)
+
+q = Question.create(question: 'Método utilizado para imprimir algún texto a pantalla', diagnostic_exam_id: d.id)
+Answer.create(answer: 'Console.WriteLine()', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: 'Console.Print()', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'System.WriteLine()', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: 'System.Print()', field_type: 'radio_button', correct: false, question_id: q.id)
+
+q = Question.create(question: 'x = 3 / 2, ¿Cuál es el valor de x?', diagnostic_exam_id: d.id)
+Answer.create(answer: '1', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: '1.5', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: '2', field_type: 'radio_button', correct: false, question_id: q.id)
+
+q = Question.create(question: 'x = 3 / 2, ¿Cuál es el valor de x?', diagnostic_exam_id: d.id)
+Answer.create(answer: '1', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: '1.5', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: '2', field_type: 'radio_button', correct: false, question_id: q.id)
+
+q = Question.create(question: 'x = 3 / 2.0, ¿Cuál es el valor de x?', diagnostic_exam_id: d.id)
+Answer.create(answer: '1', field_type: 'radio_button', correct: false, question_id: q.id)
+Answer.create(answer: '1.5', field_type: 'radio_button', correct: true, question_id: q.id)
+Answer.create(answer: '2', field_type: 'radio_button', correct: false, question_id: q.id)
+
+#
+# puts 'Creando Tareas...'
+# homeworks = Dir[File.join('db/homeworks/*')].sort
+#
+# homeworks.each do |homework|
+#   subject, level = File.basename(homework).split('-')
+#   Homework.create(subject: subject, level: level, description: File.read(homework)) if Homework.where(subject: subject, level: level, description: File.read(homework)).empty?
+# end
+#
+# puts 'Creando Videos...'
+# videos = [
+#   {
+#     url: '//www.youtube.com/embed/T4RPkiXjqd8',
+#     subject: 'ifs',
+#     title: 'Estatuos de Decision #1'
+#   },
+#   {
+#     url: '//www.youtube.com/embed/iFJ8xRgRoXg',
+#     subject: 'switches',
+#     title: 'Estatuos de Decision #2'
+#   },
+#   {
+#     url: '//www.youtube.com/embed/xAQiw-uoNeE',
+#     subject: 'cycles',
+#     title: 'Ciclos #1'
+#   },
+#   {
+#     url: '//www.youtube.com/embed/OfT0585DZUg',
+#     subject: 'cycles',
+#     title: 'Ciclos #2'
+#   }
+# ]
+#
+# videos.each do |video|
+#   Video.create(video) if Video.where(video).empty?
+# end
