f294b6a8b2597bddced5de8b807f5ba2824456dd
Adrian Rangel <adrian.rangel@gmail.com>
2014-12-07 21:29:21 -0600

    set video url on model

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -37,8 +37,6 @@ class Admin::VideosController < Admin::ApplicationController
   end
 
   def video_params
-    video_code = %r{^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*}.match(params[:video][:url])[2]
-    params[:video][:url] = "//www.youtube.com/embed/#{video_code}"
     params.require(:video).permit(:subject, :url, :title)
   end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,10 @@
 class Video < ActiveRecord::Base
+  before_create :use_embeded_video
+
+  private
+
+  def use_embeded_video
+    video_code = %r{^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*}.match(url)[2]
+    self.url = "//www.youtube.com/embed/#{video_code}"
+  end
 end
