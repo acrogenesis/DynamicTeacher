6556217fab1a859e5e7f56a3fe65ad4151d0a02e
Adrian Rangel <adrian.rangel@gmail.com>
2014-12-07 21:17:54 -0600

    add markdown to questions.question

---
+++ db/migrate/20141207225053_change_column_question_on_question.rb
@@ -0,0 +1,9 @@
+class ChangeColumnQuestionOnQuestion < ActiveRecord::Migration
+  def up
+    change_column :questions, :question, :text
+  end
+
+  def down
+    change_column :questions, :question, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141207114135) do
+ActiveRecord::Schema.define(version: 20141207225053) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 20141207114135) do
   end
 
   create_table "questions", force: true do |t|
-    t.string   "question"
+    t.text     "question"
     t.integer  "diagnostic_exam_id"
     t.datetime "created_at"
     t.datetime "updated_at"
